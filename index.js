46bb07b319219667b6a64852fbd7106c4568baea
jaybajaj <jay.bajaj2451@gmail.com>
2017-01-07 17:03:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,31 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
 
+
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
 // Server frontpage
 app.get('/', function (req, res) {
     res.send('This is TestBot Server');
 });
+
+
+
 
 // Facebook Webhook
 app.get('/webhook', function (req, res) {
