2dd0829c8e3525e270eca365dd9976be8c6d361e
jaybajaj <jay.bajaj2451@gmail.com>
2017-01-07 17:07:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            sendMessage(event.sender.id, {text: "sent from bot: " + event.message.text});
         }
     }
     res.sendStatus(200);
